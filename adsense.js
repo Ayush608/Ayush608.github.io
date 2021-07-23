a0db64590c96709fb49a79b960e1c65499abae54
Ayush608 <72180236+Ayush608@users.noreply.github.com>
2021-07-23 18:34:07 +0530

    Update adsense.js

--- adsense.js
+++ adsense.js
@@ -1,5 +1,6 @@
 // adsense script
-data-ad-client="ca-pub-1525045383348447" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+    document.body.append(script);
+    script.innerHTML = 'console.log('fine');'
 // in articles ads 
 var article_ads ='<ins class="adsbygoogle"'+
      'style="display:block; text-align:center;"'+
