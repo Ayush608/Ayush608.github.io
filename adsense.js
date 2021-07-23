c1445e8172149f66b669f2b9cf161cc661094423
Ayush608 <72180236+Ayush608@users.noreply.github.com>
2021-07-23 16:53:57 +0530

    Update adsense.js

--- adsense.js
+++ adsense.js
@@ -1,15 +1,11 @@
 // in articles ads 
 var article_ads ='<!-- ads starting -->'+
-'<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>'+
 '<ins class="adsbygoogle"'+
      'style="display:block; text-align:center;"'+
      'data-ad-layout="in-article"'+
      'data-ad-format="fluid"'+
      'data-ad-client="ca-pub-1525045383348447"'+
      'data-ad-slot="3060535157"></ins>'+
-'<script>'+
-     '(adsbygoogle = window.adsbygoogle || []).push({});'+
-'</script>'+
 '<!-- ads ending -->'
 
 // side-bar ads desktop
